47794c39ac1ef84e06f829fe34d701e5231645ff
Zytronium <dastelljes@outlook.com>
2024-08-07 13:34:41 -0500

    set up task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending
+ * order using the Bubble sort algorithm
+ *
+ * @array:
+ * @size:
+ */
+void bubble_sort(int *array, size_t size)
+{
+
+}
--- main.c
+++ main.c
@@ -42,8 +42,21 @@ void print_list(const listint_t *list)
 	printf("\n");
 }
 
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-	printf("Hello, World!\n");
-	return 0;
+	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+	size_t n = sizeof(array) / sizeof(array[0]);
+
+	print_array(array, n);
+	printf("\n");
+	bubble_sort(array, n);
+	printf("\n");
+	print_array(array, n);
+	return (0);
 }
