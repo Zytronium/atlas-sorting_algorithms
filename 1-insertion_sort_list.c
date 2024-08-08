38c325ad852fe61738722d9485fa2f4117b5ed9e
Zytronium <dastelljes@outlook.com>
2024-08-08 12:47:20 -0500

    almost finished task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,22 +8,30 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *lastElmnt;
+	listint_t *current, *last;
 
 	current = (*list)->next;
-	lastElmnt = current->prev;
+	last = current->prev;
 
 	while (current->next)
 	{
-		if (current->prev->n > current->n)
+		while (last != NULL && last->n > current->n)
 		{
-			/* copy current node to temp node */
-			listint_t temp = *current;
 			/* shift last element over to right */
-			lastElmnt->
+			if (last->prev != NULL)
+				last->prev->next = current;
+			if (current->next != NULL)/*may not need this bc of 1st loop cndtn*/
+				current->next->prev = last;
+			current->prev = last->prev;
+			last->prev = current;
+			last->next = current->next;
+			current->next = last;
 
+			last = current->prev;
+			/* might need to find and set the new head */
+			print_list(*list);
 		}
 		current = current->next;
-		lastElmnt = current->prev;
+		last = current->prev;
 	}
 }
