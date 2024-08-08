666c2980698f4713e76ec1b6a3e2671a70e8377c
Zytronium <dastelljes@outlook.com>
2024-08-08 13:47:09 -0500

    started task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in
+ * ascending order using the Selection sort algorithm
+ * @array: an array of numbers to sort
+ * @size: the size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		int idxOfMin = i, j;
+
+		for (j = 1; j < size; j++) /* find min num in remaining numbers */
+		{
+			if (array[j] < array[idxOfMin])
+				idxOfMin = j;
+		}
+		swap_ints(&array[i], &array[idxOfMin]);
+		print_array(array, size);
+	}
+}
+
+/**
+ * swap_ints - swaps 2 numbers in an array
+ * helper function for selection_sort
+ *
+ * @a: the number to swap with a
+ * @b: the number to swap with b
+ */
+void swap_ints(int *a, int *b)
+{
+	int temp = *a;
+
+	*a = *b;
+	*b = temp;
+}
